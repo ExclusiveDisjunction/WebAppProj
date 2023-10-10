8ccdebf354ecd38f2fb44319cb1f22b37b1c2084
Hollan Sellars <exclusivedisjunction@outlook.com>
2023-10-10 19:42:26 -0400

    Finished SitemapUpdate, and published a release for windows.

--- SitemapUpdate/sitemapupdate.cpp
+++ SitemapUpdate/sitemapupdate.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <time.h>
 #include <fstream>
+#include <filesystem>
 
 #include <unistd.h>
 #include <limits.h>
@@ -22,6 +23,58 @@ int main()
     string SitemapPath;
     cout << "Please enter the path of the sitemap: ";
     getline(cin, SitemapPath);
+
+    cout << endl;
+
+    string Url, DateStr;
+
+    //Determine the Url from the url.txt file.
+    {
+        filesystem::path CurrentPath(cwd);
+        filesystem::path ActiveDirectoryName = CurrentPath.filename();
+        filesystem::path UrlDestFile;
+        if (ActiveDirectoryName != "SitemapUpdate")
+            UrlDestFile = CurrentPath / "SitemapUpdate/url.txt";
+        else
+            UrlDestFile = CurrentPath / "url.txt";
+
+        cout << "Finding url file at " << UrlDestFile << endl;
+        ifstream UrlPath(UrlDestFile);
+        if (!UrlPath)
+        {
+            cout << "Error with reading url file, please check that SitemapUpdate/url.txt exists." << endl;
+            return 1; 
+        }
+
+        getline(UrlPath, Url);
+        Url += "index.html";
+        UrlPath.close();
+    }
+
+    //Determine the date string.
+    {
+        time_t CurrentTime = time(nullptr);
+        tm CurrentLocalTime = *localtime(&CurrentTime);
+        DateStr = to_string(CurrentLocalTime.tm_year + 1900) + "-" + to_string(CurrentLocalTime.tm_mon + 1) + "-" + to_string(CurrentLocalTime.tm_mday);
+    }
+
+    ofstream OutFile(SitemapPath);
+    if (!OutFile)
+    {
+        cout << "Could not open the sitemap file provided." << endl;
+        return 1;
+    }
+
+    OutFile <<  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << endl << 
+                "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" << endl << 
+                "\t<url>" << endl <<
+                "\t\t<loc>" << Url << "</loc>" << endl << 
+                "\t\t<lastmod>" << DateStr << "</lastmod>" << endl <<
+                "\t</url>" << endl <<
+                "</urlset>" << endl;
+
+    cout << "The file has been updated to have url \"" << Url << "\" and timestamp " << DateStr << "." << endl;
+    OutFile.close();
 
     return 0;
 }
