1c2766aa08091cb226d5827c890e78a9a9433674
ExDisjunction <exdisj@outlook.com>
2023-10-11 09:31:54 -0400

    Progress on Navbar Inejctor.

--- js/NavbarInject.js
+++ js/NavbarInject.js
@@ -6,12 +6,10 @@ request.onload = function()
     var reader = new FileReader();
     reader.readAsText(request.response);
     reader.onload = function(e)
-    {
-        console.log('DataURL:', e.target.result);
-        
+    {        
         const template = document.createElement('template');
         template.innerHTML = e.target.result;
-        Header = document.getElementsByTagName("header");
+        var Header = document.getElementsByTagName("header");
         if (Header.length != 1)
         {
             alert("Error, could not find header.");
