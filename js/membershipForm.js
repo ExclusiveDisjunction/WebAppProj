edd234bd33044fdd29ef1f31e7c8a0e3e07d3c5f
Hollan Sellars <exclusivedisjunction@outlook.com>
2023-10-18 23:29:39 -0400

    Worked on styles, index, and js for membershipform. Some validations still need to be completed.

--- js/membershipForm.js
+++ js/membershipForm.js
@@ -50,16 +50,24 @@ document.addEventListener("DOMContentLoaded", function()
             else
                 lastName.value = capitalizeEachWord(lastName.value);
 
+            //Todo: Finish phone number, email, and password validation.
+
             if (errorFields.length > 0)
             {
-                let ErrorMessage = "Please Correct the following issues: ";
+                let ErrorMessage = "<h2>Please Correct the following issues:</h2>\n";
                 for (let error of errorFields)
                 {
                     error.element.classList.add("errorField");
-                    ErrorMessage += "\n" + error.message;
+                    ErrorMessage += "<p>    " + error.message + "</p>\n";
                 }
 
-                
+                const errorOutput = document.getElementById("errorOutput");
+                if (errorOutput)
+                {
+                    errorOutput.innerHTML = ErrorMessage;
+                    errorOutput.classList.remove("hidden-elem");
+                    errorOutput.classList.add("visible-elem")                    
+                }
             }
         });
     }
@@ -68,10 +76,14 @@ document.addEventListener("DOMContentLoaded", function()
     {
         resetBttn.addEventListener("click", function()
         {
-            firstName.value = "";
-            lastName.value = "";
-            phoneNumber.value = "";
-            email.value = "";
+            const cssSelector = "input[type=text],input[type=password]";
+            const fields = document.querySelectorAll(cssSelector);
+
+            for (let field of fields)
+            {
+                field.value = "";
+                field.classList.remove("errorField");
+            }
 
             const radioButtons = document.getElementsByName("subStatus");
             if (radioButtons && radioButtons.length >= 1)
@@ -79,6 +91,14 @@ document.addEventListener("DOMContentLoaded", function()
                 for (let radiobttn of radioButtons)
                     radiobttn.checked = false;
                 radioButtons[0].checked = true;
+            }
+
+            const errorOutput = document.getElementById("errorOutput");
+            if (errorOutput)
+            {
+                errorOutput.classList.remove("visible-elem");
+                errorOutput.classList.add("hidden-elem");         
+                errorOutput.innerHTML = "";       
             }
         });
     }
